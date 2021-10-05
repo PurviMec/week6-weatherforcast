a19e7180f1e8219e80c4e290d0905d9603f0fe4e
PurviMec <princy.mecwan94@gmail.com>
2021-10-05 15:47:56 -0400

    retrive current weather

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,48 +1,40 @@
 let inputEl = document.querySelector('#input-field');
 let onClickEl = document.querySelector('#btn');
 let userFormEl = document.querySelector('#userForm');
-let todaysWeatherEl = document.querySelector('#todays-weather');
 let futureDayWeatherEl = document.querySelector('#future-weather');
+let currentWeatherEl = document.querySelector('#current-weather-data');
+let cityNameEl = document.querySelector('#city-name');
 
 const m = moment();
-document.getElementById('city-name').innerHTML = moment().format('l');  
+document.getElementById('currentDate').innerHTML = moment().format('l');  
 document.getElementById('date1').innerHTML =  moment(moment(), "DD-MM-YYYY").add(1, 'days').format('l'); 
 document.getElementById('date2').innerHTML =  moment(moment(), "DD-MM-YYYY").add(2, 'days').format('l'); 
 document.getElementById('date3').innerHTML =  moment(moment(), "DD-MM-YYYY").add(3, 'days').format('l'); 
 document.getElementById('date4').innerHTML =  moment(moment(), "DD-MM-YYYY").add(4, 'days').format('l'); 
 document.getElementById('date5').innerHTML =  moment(moment(), "DD-MM-YYYY").add(5, 'days').format('l'); 
 
+var getForecast = function() {
 
-var getForecast = function(lat, long) {
-
-    
-    var dailyUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=43.159374&lon=-79.246864&exclude=hourly,current,minutely&units=metric&appid=93712e51c9b96dd6f82b519a86a2a96b";
+    navigator.geolocation.getCurrentPosition((success) => {
+         
+        let {latitude, longitude} = success.coords;
      
-    fetch(dailyUrl).then(function(response){
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=93712e51c9b96dd6f82b519a86a2a96b`)
+    .then(function(response){
         if (!response.ok){
             error(ERROR)
         }
          return response.json();
     })
     .then(data => {
-         console.log(data.daily[0]);
-
-         const html = data.daily.map(daily => {
-             return `<p>Temp: ${daily.temp.day}°C</P>
-             <p>Wind: ${daily.wind_speed}MPH</P>
-             <p>Humidity: ${daily.humidity}%</P>
-             <p class = 'status_data'>UV Index: ${daily.uvi}</P>
-             `
-             
-         }).join(''); 
-         console.log(html);
-         document.querySelector('#current-weather-data').innerHTML = html;
-        
+         console.log(data);
+        displayData(data);
+         
          
     }).catch(error=>{
         console.log(error);
     });
-    
+    })
 
 }
 getForecast();
@@ -62,7 +54,40 @@ var searchButtonHandler = function(event){
 
 };
 
+function displayData(data){
+    let{timezone} = data;
 
+    cityNameEl.innerHTML = 
+    `
+    <div class="current-weather-data">
+        <div>
+            <h3> ${timezone}<span id = "icon"></span></h3>
+        </div>
+    </div> 
+    `;
+    
+    let{temp, wind_speed, humidity, uvi} = data.current;
+    
+    currentWeatherEl.innerHTML =
+    
+    `
+    <div class="current-weather-data">
+        <div>Temp: ${temp}</div>
+    </div>    
+    <div class="current-weather-data">
+        <div>Wind: ${wind_speed}</div>
+    </div>    
+    <div class="current-weather-data">
+        <div>Humidity: ${humidity}</div>
+    </div>    
+    <div class="current-weather-data">
+        <div>UV Index: ${uvi}</div>
+    </div>    
+    `;
+    
+
+    
+}
 
 
 
