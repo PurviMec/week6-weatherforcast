fee0149207707b514a02696e43c7836cc483ebf9
PurviMec <princy.mecwan94@gmail.com>
2021-10-14 20:57:17 -0400

    commit

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,15 +1,15 @@
-let inputEl = document.querySelector('#input-field');
-let onClickEl = document.querySelector('#btn');
-let userFormEl = document.querySelector('#userForm');
-let futureDayWeatherEl = document.querySelector('#future-weather');
-let currentWeatherEl = document.querySelector('#current-weather-data');
+var inputEl = document.querySelector('#input-field');
+var onClickEl = document.querySelector('#btn');
+var userFormEl = document.querySelector('#userForm');
+var futureDayWeatherEl = document.querySelector('#future-weather');
+var currentWeatherEl = document.querySelector('#current-weather-data');
 
 const m = moment();  
 
     
 $('#btn').on('click', function(event) {
     event.preventDefault();
-    
+    $("input[id='input-field']").html("");
     var name = document.querySelector("input[id='input-field']").value;
    
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + name + '&appid=93712e51c9b96dd6f82b519a86a2a96b')
@@ -22,11 +22,16 @@ $('#btn').on('click', function(event) {
             return weather.json(response);
         })
         .then(function(weather) {
+            
             console.log(response);
             console.log(weather);
             saveCity();
             displayCity(name, weather);
-            futureWeatherData(weather);
+            displayFuture(weather);
+            displayFuture2(weather);
+            displayFuture3(weather);
+            displayFuture4(weather);
+            displayFuture5(weather);
             
         })
     })
@@ -50,16 +55,16 @@ var displaySearch = function(getCityName){
     .addClass('btn btn-secondary m-2 col-12')
     $(searchEl).append(searchCityNameEl);
     
-    for (i = 0; i < searchCityNameEl.length; i++){
+    for (var i = 0; i < searchCityNameEl.length; i++){
         cityNameBtn(searchCityNameEl, i);
     }
 }
 
 var displayCity = function(name, weather){
-    let icon = weather.current.weather[0].icon;
-    let today = window.moment().format('l');
-    let searchCityEl = document.getElementById("city-date-icon");
-    let geticonEl = document.getElementById('icon')
+    //var icon = weather.current.weather[0].icon;
+    var today = window.moment().format('l');
+    var searchCityEl = document.getElementById("city-date-icon");
+    //let geticonEl = document.getElementById('icon');
 
     var getCityEl = $('<span></span>')
     .text (name)
@@ -69,16 +74,12 @@ var displayCity = function(name, weather){
     .text (today)
     .addClass('m-2')
     
-    
-    geticonEl.innerHTML = `
-    <span><img src = "http://openweathermap.org/img/wn/${icon}@2x.png"</span>
-    `;
-
-    let{temp, wind_speed, humidity, uvi} = weather.current;
+    var{weather, temp, wind_speed, humidity, uvi} = weather.current;
     
     currentWeatherEl.innerHTML =
     
     `
+    <span><img src = "http://openweathermap.org/img/wn/${weather[0].icon}@2x.png"</span>
     <div class="current-weather-data">
         <div>Temp: ${temp}</div>
     </div>    
@@ -98,54 +99,64 @@ var displayCity = function(name, weather){
     $(searchCityEl).append(getCityEl, getdayEl);
 }
 
-var futureWeatherData = function(weather){
-    for (i=0; i< weather.daily.length; i++){
-        displayFuture (weather, i);
-    }
+var displayFuture = function (weather){
+    let forcastDay1El = document.getElementById('date1');
+    let {temp, wind_speed, humidity} = weather.daily[1];
+    let icon = weather.daily[1].weather[0].icon;
+    forcastDay1El.innerHTML = `
+        <div class="date" id="date1"><h4>${window.moment(moment(), "DD-MM-YYYY").add(1, 'days').format('l')}</h4></div>
+        <div><span><img src = "http://openweathermap.org/img/wn/${icon}@2x.png"</span></div>
+        <div class="temp">Temp: ${temp.day}</div>
+        <div class="wind">Wind: ${wind_speed}</div>
+        <div class="humidity">Humidity: ${humidity}</div>
+   `;
 }
 
-var displayFuture = function (weather, i){
-    let forcastDay1El = document.getElementById('date1');
+var displayFuture2 = function ( weather){
     let forcastDay2El = document.getElementById('date2');
+    let {temp, wind_speed, humidity} = weather.daily[2];
+    let icon = weather.daily[2].weather[0].icon;
+    forcastDay2El.innerHTML = `
+        <div class="date" id="date1"><h4>${window.moment(moment(), "DD-MM-YYYY").add(2, 'days').format('l')}</h4></div>
+        <div><span><img src = "http://openweathermap.org/img/wn/${icon}@2x.png"</span></div>
+        <div class="temp">Temp: ${temp.day}</div>
+        <div class="wind">Wind: ${wind_speed}</div>
+        <div class="humidity">Humidity: ${humidity}</div>
+   `;
+}
+
+var displayFuture3 = function ( weather){
     let forcastDay3El = document.getElementById('date3');
-    let forcastDay4El = document.getElementById('date4');
-    let forcastDay5El = document.getElementById('date5');
-    let {date, temp, wind_speed, humidity} = weather.daily[i];
-    let icon = weather.daily[i].weather[0].icon;
-    forcastDay1El.innerHTML = `
-        <div class="date" id="date1"><h4>${window.moment(moment(date), "DD-MM-YYYY").add(1, 'days').format('l')}</h4></div>
-        <div><span><img src = "http://openweathermap.org/img/wn/${icon}@2x.png"</span></div>
-        <div class="temp">Temp: ${temp.day}</div>
-        <div class="wind">Wind: ${wind_speed}</div>
-        <div class="humidity">Humidity: ${humidity}</div>
-   `;
-
-   forcastDay2El.innerHTML = `
-        <div class="date" id="date1"><h4>${window.moment(moment(date), "DD-MM-YYYY").add(2, 'days').format('l')}</h4></div>
-        <div><span><img src = "http://openweathermap.org/img/wn/${icon}@2x.png"</span></div>
-        <div class="temp">Temp: ${temp.day}</div>
-        <div class="wind">Wind: ${wind_speed}</div>
-        <div class="humidity">Humidity: ${humidity}</div>
-   `;
-
+    let {temp, wind_speed, humidity} = weather.daily[3];
+    let icon = weather.daily[3].weather[0].icon;
    forcastDay3El.innerHTML = `
-        <div class="date" id="date1"><h4>${window.moment(moment(date), "DD-MM-YYYY").add(3, 'days').format('l')}</h4></div>
+        <div class="date" id="date1"><h4>${window.moment(moment(), "DD-MM-YYYY").add(3, 'days').format('l')}</h4></div>
         <div><span><img src = "http://openweathermap.org/img/wn/${icon}@2x.png"</span></div>
         <div class="temp">Temp: ${temp.day}</div>
         <div class="wind">Wind: ${wind_speed}</div>
         <div class="humidity">Humidity: ${humidity}</div>
    `;
+}
 
+var displayFuture4 = function (weather){
+    let forcastDay4El = document.getElementById('date4');
+    let {temp, wind_speed, humidity} = weather.daily[4];
+    let icon = weather.daily[4].weather[0].icon;
    forcastDay4El.innerHTML = `
-        <div class="date" id="date1"><h4>${window.moment(moment(date), "DD-MM-YYYY").add(4, 'days').format('l')}</h4></div>
+        <div class="date" id="date1"><h4>${window.moment(moment(), "DD-MM-YYYY").add(4, 'days').format('l')}</h4></div>
         <div><span><img src = "http://openweathermap.org/img/wn/${icon}@2x.png"</span></div>
         <div class="temp">Temp: ${temp.day}</div>
         <div class="wind">Wind: ${wind_speed}</div>
         <div class="humidity">Humidity: ${humidity}</div>
    `;
+}
 
+var displayFuture5 = function ( weather){
+    let forcastDay5El = document.getElementById('date5');
+    let {temp, wind_speed, humidity} = weather.daily[5];
+    let icon = weather.daily[5].weather[0].icon;
    forcastDay5El.innerHTML = `
-        <div class="date" id="date1"><h4>${window.moment(moment(date), "DD-MM-YYYY").add(5, 'days').format('l')}</h4></div>
+        <div class="date" id="date1"><h4>${window.moment(moment(), "DD-MM-YYYY").add(5, 'days').format('l')}</h4></div>
         <div><span><img src = "http://openweathermap.org/img/wn/${icon}@2x.png"</span></div>
         <div class="temp">Temp: ${temp.day}</div>
         <div class="wind">Wind: ${wind_speed}</div>
@@ -157,13 +168,13 @@ var displayFuture = function (weather, i){
 var cityNameBtn = function (searchCityNameEl, i){
     $(searchCityNameEl[i]).on('click', function(event) {
         event.preventDefault();
-        //document.getElementById('date1').innerHTML= "";
-        //document.getElementById('date2').innerHTML = "";
-        //document.getElementById('date3').innerHTML = "";
-        //document.getElementById('date4').innerHTML= "";
-        //document.getElementById('date5').innerHTML = "";
-        //$('#city-date-icon').html("");
-        //document.getElementById('current-weather-data').innerHTML = "";
+        document.getElementById('date1').innerHTML= "";
+        document.getElementById('date2').innerHTML = "";
+        document.getElementById('date3').innerHTML = "";
+        document.getElementById('date4').innerHTML= "";
+        document.getElementById('date5').innerHTML = "";
+        $('#city-date-icon').html("");
+        document.getElementById('current-weather-data').innerHTML = "";
         
         var name = document.querySelector("input[id='input-field']").value;
    
@@ -177,12 +188,15 @@ var cityNameBtn = function (searchCityNameEl, i){
                 return weather.json(response);
             })
             .then(function(weather) {
-                console.log(response);
-                console.log(weather);
-                saveCity();
+                document.querySelector("input[id='input-field']").value = '';
+                //console.log(response);
+                //console.log(weather);
                 displayCity(name, weather);
-                futureWeatherData(weather);
-                
+                displayFuture(weather);
+                displayFuture2(weather);
+                displayFuture3(weather);
+                displayFuture4(weather);
+                displayFuture5(weather);
             })
         })
     })
